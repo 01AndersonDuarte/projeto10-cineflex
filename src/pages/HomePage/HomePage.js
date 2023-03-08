c1c51453fe21de534c5c184348e868c8b225c819
Anderson Duarte <andersonduarte997@gmail.com>
2023-03-08 15:51:43 -0300

    Requisito buscar informações dos filmes na API implementado

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
 import styled from "styled-components"
 
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        const promise = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies");
+        
+        promise.then((resposta)=>{
+            console.log(resposta.data);
+            setMovies(resposta.data)
+        });
+        promise.catch((resposta)=>{
+            console.log(resposta);
+        });
+    }, []);
+    
     return (
         <PageContainer>
             Selecione o filme
 
             <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+                {movies.map((m)=><MovieContainer key={m.id}><img src={m.posterURL} alt="movie-poster"/></MovieContainer>)}
             </ListContainer>
 
         </PageContainer>
